b94d809764e6eb6069cdf14ced6121d805451c1d
torgav <christian.torresgaviria@elev.ga.ntig.se>
2024-05-21 14:47:28 +0200

    SKrev om min main för att anpassa mer för syftet

--- ChristianClient.java
+++ ChristianClient.java
@@ -1,41 +1,50 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner; 
+
 
 public class ChristianClient {
 
-    public static void main(String args[])
-            throws Exception {
-
-
-        // Default port number we are going to use
-        int portnumber = 5000;
-        if (args.length >= 1) {
-            portnumber = Integer.parseInt(args[0]);
+    public class Client {
+        public static void main(String[] args) {
+            // Server's IP address or hostname and port number to connect to
+            String hostName = "localhost"; // Typically the server's address
+            int portNumber = 4446; // The port on which the server is listening
+    
+            try (
+    
+                Socket socket = new Socket(hostName, portNumber); // Establish a connection to the server
+    
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);// Output stream to send data to the server
+    
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Input stream to read data from the server
+    
+                Scanner scanner = new Scanner(System.in)
+            ) {
+                String userInput;
+                // Prompt the user to enter math expressions to send to the server
+                System.out.println("\n\nConnected to the server. Type your math expressions (e.g., '1 + 2') to send.");
+                while (true) {
+                    System.out.print("Enter expression (or 'quit' to exit): ");
+                    userInput = scanner.nextLine(); 
+    
+                    if ("quit".equalsIgnoreCase(userInput)) { 
+                        break; 
+                    }
+    
+                    out.println(userInput); // Send the user input to the server
+    String response = in.readLine(); // Read the server's response
+                    System.out.println("Server response: " + response); // Display the server's response
+                }
+            } catch (UnknownHostException e) {
+                // Handle error if the host's IP address could not be determined
+                System.err.println("Don't know about host " + hostName);
+                System.exit(1);
+            } catch (IOException e) {
+                // Handle general input/output errors (e.g., network errors)
+                System.err.println("Couldn't get I/O for the connection to " + hostName);
+                System.exit(1); 
+            }
         }
-
-        // Create a MulticastSocket
-        MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
-
-        // Determine the IP address of a host, given the host name
-        InetAddress group =
-                InetAddress.getByName("225.4.5.6");
-
-        // Joins a multicast group
-        chatMulticastSocket.joinGroup(group);
-
-        // Prompt a user to enter a message
-        String msg = "";
-        System.out.println("Type a message for the server:");
-        BufferedReader br =
-                new BufferedReader(new InputStreamReader(System.in));
-        msg = br.readLine();
-
-        // Send the message to Multicast address
-        DatagramPacket data = new DatagramPacket(msg.getBytes(), 0,
-                msg.length(), group, portnumber);
-        chatMulticastSocket.send(data);
-
-        // Close the socket
-        chatMulticastSocket.close();
     }
 }
